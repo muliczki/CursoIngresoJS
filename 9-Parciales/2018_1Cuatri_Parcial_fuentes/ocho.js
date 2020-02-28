28f07dd4ad5b55e7507c9a5f2702412ba8956b9e
muliczki <60760201+muliczki@users.noreply.github.com>
2020-02-28 16:51:10 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,22 +1,71 @@
 /*Enunciado:
 Bienvenidos.
-Realizar el algoritmo que permita el ingreso por prompt de las notas 
-(validar entre 0 y 10) , el sexo (validar el sexo “f” o “m”) de 5 alumnos, 
-informar por alert:
-a) El promedio de las notas totales.
-b) La nota más baja y el sexo de esa persona.
-c) La cantidad de varones que su nota haya sido mayor o igual a 6.*/
+Realizar el algoritmo que permita iterar el ingreso de dos datos, 
+una letra y un número entre -100 y 100 (validar) 
+hasta que el usuario quiera e informar al terminar el 
+ingreso por document.write:
+a) La cantidad de números pares.
+b) La cantidad de números impares.
+c) La cantidad de ceros.
+d) El promedio de todos los números positivos ingresados.
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.
+
+Testeo con los siguientes datos
+( d , -150(mal) , -50(bien))
+( z , 0)
+( g , 20)
+( a , 150(mal) , 0(bien))
+( b , 3)
+( k , 7)*/
+
 
 function mostrar()
 {
-var nota;
-var sexo;
+var i=1
+var letra
+var numero
+var respuesta="si"
+var contadorpares=0
+var contadorimpares=0
+var contadorceros=0
 
-nota = parseInt(prompt("Ingrese una nota de 0 a 10"));
-/*while(nota<0 || nota>10 || isNaN(nota))
+do{
+letra = prompt("Ingrese una letra");
+while(isNaN(letra)==false) /*COMO VALIDO QUE SEA UN SOLO CARACTER*/
 {
-    nota=parseInt(prompt("Nota incorrecta. Ingrese una nota de 0 a 10"))
+    letra = prompt("Error. Ingrese una letra")
+}
+numero = parseInt(prompt("Ingrese un numero de -100 a 100"));
+while(numero<-100 || numero>100 || isNaN(numero))
+{
+    numero = parseInt(prompt("Error. Ingrese un numero de -100 a 100"));
 }
 
-sexo= prompt("Ingrese el sexo: f/m")*/
+if(numero!=0 && numero%2==0) /*OK?? O COMO SACO EL CERO. EL CER0 ES PAR?*/
+{
+    contadorpares++
+
+}else if(numero==0)
+{
+    contadorceros++
+}
+
+else
+{
+contadorimpares++
+}
+respuesta = prompt("Querés ingresar otra letra y otro numero? si/no")
+while(respuesta!="si" && respuesta!="no")
+{
+    respuesta = prompt("Error. Querés ingresar otra letra y otro numero? si/no");
+}
+}while(respuesta=="si");
+
+
+document.write("Cantidad de numeros pares: " +contadorpares +"<br/>"+
+"Cantidad de numeros impares: " +contadorimpares +"<br/>"+
+"Cantidad de numeros ceros: " +contadorceros +"<br/>")
+
+
 }
