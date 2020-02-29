377000b9d7c20fe9ec2e345daf40d4bd8aaa9cdb
muliczki <60760201+muliczki@users.noreply.github.com>
2020-02-28 21:34:32 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,112 @@
+/*
+Ingrese datos
+Mostrar información
+Enunciado:
+Bienvenidos.
+Realizar el algoritmo que permita ingresar la marca del producto, 
+el peso el cual debe ser entre 1 y 100 (validar),
+la temperatura de almacenamiento(entre -30 y 30) 
+hasta que el usuario quiera e informar al terminar el ingreso por document.write:
+a) La cantidad de temperaturas pares.
+b) La marca del producto más pesado
+c) La cantidad de productos que se conservan a menos de 0 grados.
+d) El promedio del peso de todos los productos.
+f) El peso máximo y el mínimo.
+
+Testeo con los siguientes valores
+(m=pepito;p = 50; t =-300(mal) ,-30(bien) )
+(m=teem;p = 10; t =0 )
+(m=llut;p = 150(mal), 15(bien); t =-13 )
+(m=fpy;p = 45; t =-12 )*/
+
 function mostrar()
 {
+var marca;
+var peso;
+var temp;
+var rta="s"
+var temppares=0;
+var pesomax;
+var marcamax;
+var pesomin;
+var flag=0;
+var bajocero=0;
+var sumapeso=0;
+var cantidadproductos=0
+var promedio;
+
+do{
+marca=prompt("Ingrese la marca");
+while(!isNaN(marca))
+{
+    marca=prompt("Error. Ingrese la marca");
+}
+
+peso =parseInt(prompt("Ingrese el peso entre 1 y 100"));
+while(peso>100 || peso<1 || isNaN(peso) )
+{
+    peso =parseInt(prompt("Error. Ingrese el peso entre 1 y 100"));
+}
+
+temp =parseInt(prompt("Ingrese la temperatura entre -30 y 30"));
+while(temp>30 || temp<-30 || isNaN(temp) )
+{
+    temp =parseInt(prompt("Error. Ingrese la temperatura entre -30 y 30"));
+}
+
+//-------------------temperaturas pares----------------------------
+if(temp%2==0)
+{
+temppares++
+}
+
+//-------------------marca prod mas pesado // peso max y min----------------------
+
+if(flag==0 || peso>pesomax)
+{
+    pesomax=peso
+    marcamax=marca
+}
+if( flag==0 || peso<pesomin) 
+    {
+        pesomin=peso
+        flag=1
+
+}
+//-------------------temperaturas menores a cero----------------------------
+if(temp<0)
+{
+bajocero++
+}
+//------------------------------------------------------------------------
+cantidadproductos++;
+sumapeso=sumapeso+peso;
+
+rta=prompt("Desea agregar otro producto? s/n");
+while(rta!="s" && rta!="n")
+{
+    rta=prompt("Error. Desea agregar otro producto? s/n");
+}
+}while(rta=="s")
+
+if(cantidadproductos==0)
+{
+    promedio=0
+}
+else 
+{
+    promedio=sumapeso/cantidadproductos
+}
+
+document.write("a) Cantidad de temperaturas pares: "+temppares+"</br>"+
+"b) La marca del producto más pesado: "+ marcamax +"</br>"+
+"c) La cantidad de productos a menos de 0 grados: "+ bajocero +"</br>"+
+"d) El promedio del peso de todos los productos: "+promedio+ "</br>"+
+"e) El peso máximo: "+ pesomax +"</br>"+
+"f) El peso mínimo: "+ pesomin + "</br>")
+
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -20,52 +20,87 @@ Testeo con los siguientes datos
 ( k , 7)*/
 
 
-function mostrar()
-{
-var i=1
-var letra
-var numero
-var respuesta="si"
-var contadorpares=0
-var contadorimpares=0
-var contadorceros=0
+function mostrar() {
+    var i = 1
+    var letra
+    var numero
+    var respuesta = "si"
+    var contadorpares = 0
+    var contadorimpares = 0
+    var contadorceros = 0
+    var positivos = 0
+    var negativos = 0
+    var promedioposit = 0
+    var contadorpositivos = 0
+    var numeroMax
+    var numeroMin
+    var letraMax
+    var letraMin
+    var flag = 0
 
-do{
-letra = prompt("Ingrese una letra");
-while(isNaN(letra)==false) /*COMO VALIDO QUE SEA UN SOLO CARACTER*/
-{
-    letra = prompt("Error. Ingrese una letra")
-}
-numero = parseInt(prompt("Ingrese un numero de -100 a 100"));
-while(numero<-100 || numero>100 || isNaN(numero))
-{
-    numero = parseInt(prompt("Error. Ingrese un numero de -100 a 100"));
-}
+    do {
+        letra = prompt("Ingrese una letra");
+        while (!((letra >= "A" && letra <= "Z") || (letra >= "a" && letra <= "z")))    //ver tabla de codigo ASCII
+        //(!isNaN(letra) || letra.length !=1) 
+        {
+            letra = prompt("Error. Ingrese una letra")
+        }
+        numero = parseInt(prompt("Ingrese un numero de -100 a 100"));
+        while (numero < -100 || numero > 100 || isNaN(numero)) {
+            numero = parseInt(prompt("Error. Ingrese un numero de -100 a 100"));
+        }
+//---------------------------------------------------------
+        if (numero % 2 == 0) /*OK?? O COMO SACO EL CERO. EL CER0 ES PAR?*/ {
+            contadorpares++
+        }
 
-if(numero!=0 && numero%2==0) /*OK?? O COMO SACO EL CERO. EL CER0 ES PAR?*/
-{
-    contadorpares++
+        else {
+            contadorimpares++
+        }
+//---------------------------------------------------------
+        if (numero == 0) {
+            contadorceros++
+        }
+        else if (numero > 0) {
+            positivos = numero + positivos
+            contadorpositivos++
+        }
+        else {
+            negativos = numero + negativos
+        }
+//-----------------------------------------------------------
+        if (flag == 0 || numero > numeroMax) {
+            numeroMax = numero
+            letraMax = letra
 
-}else if(numero==0)
-{
-    contadorceros++
-}
-
-else
-{
-contadorimpares++
-}
-respuesta = prompt("Querés ingresar otra letra y otro numero? si/no")
-while(respuesta!="si" && respuesta!="no")
-{
-    respuesta = prompt("Error. Querés ingresar otra letra y otro numero? si/no");
-}
-}while(respuesta=="si");
+        }
+        if (flag == 0 || numero < numeroMin) {
+            numeroMin = numero
+            letraMin = letra
+            flag = 1
+        }
 
 
-document.write("Cantidad de numeros pares: " +contadorpares +"<br/>"+
-"Cantidad de numeros impares: " +contadorimpares +"<br/>"+
-"Cantidad de numeros ceros: " +contadorceros +"<br/>")
+        respuesta = prompt("Querés ingresar otra letra y otro numero? si/no")
+        while (respuesta != "si" && respuesta != "no") {
+            respuesta = prompt("Error. Querés ingresar otra letra y otro numero? si/no");
+        }
+    } while (respuesta == "si");
+
+    if (contadorpositivos == 0) {
+        promedioposit = 0
+    }
+    else {
+        promedioposit = positivos / contadorpositivos
+    }
+    document.write("Cantidad de numeros pares: " + contadorpares + "<br/>" +
+        "Cantidad de numeros impares: " + contadorimpares + "<br/>" +
+        "Cantidad de numeros ceros: " + contadorceros + "<br/>" +
+        "El promedio de números positivos: " + promedioposit + "<br/>" +
+        "Suma de negativos: " + negativos + "<br/>" +
+        "Maximo: " + numeroMax + " - " + letraMax + "<br/>" +
+        "Minimo: " + numeroMin + " - " + letraMin)
+
 
 
 }
